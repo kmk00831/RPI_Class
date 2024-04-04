1b6c4c3c47cdf9add9677c4f0ac9d3ea14fe5d6e
kmk00831 <kmk00831@gmail.com>
2024-04-04 10:53:29 +0900

    0404

---
+++ python_study.py
@@ -0,0 +1,40 @@
+
+
+def main():
+    print('Hello world!')   
+    
+    # Output Hello, world!!!!!
+    
+    # input
+    # key_num1, key_num2, key_num3 = input("숫자 입력해주세요.: ").split(" ")
+    # key_str = input("문자를 입력해주세요").split(",")
+    # print(f"{key_num1} {key_num2} {key_num3}")
+    
+    
+    # #2번쨰
+    # num1, num2 = map(int, input("숫자를 입력: ").split(" "))
+    
+    # print(num1 + num2)
+    
+    
+    ##### 1<x 10
+    a=1
+    b=2
+    x= int(input("x값 입력: "))
+    if 1< x and x <10:
+        print("TRUE")
+    else:
+        print("FALSE")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+      
+if __name__ == "__main__":  # 만약 코드가 실행되면 이쪽으로 감
+    main()
